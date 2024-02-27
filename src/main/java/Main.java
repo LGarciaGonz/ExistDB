c0fc903ec64eeae2ca02b54be4f0ea96dcbb7f4b
LGarciaGonz <190077@iessanalberto.com>
2024-02-27 09:44:11 +0100

    Commit final ENTREGA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import Code.ConvCSVtoXML;
+import Code.GenerarXML.GenerateCentros;
+import Code.GenerarXML.GenerateFamilias;
+import Code.GenerarXML.GenerateProyectos;
 import Code.InsertarXML;
 
 import java.util.Scanner;
@@ -33,7 +36,11 @@ public class Main {
                     InsertarXML.insertXML();
                 }
 
-//                case "2" -> ;
+                case "2" -> {
+                    GenerateCentros.CargarDatos();
+                    GenerateFamilias.CargarDatos();
+                    GenerateProyectos.CargarDatos();
+                }
 
                 default ->
                         System.err.println("\n>>> OPCIÓN NO VÁLIDA: Introduzca una opción del menú");             // Informar al usuario de un error cometido.
