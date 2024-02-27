b04ede6bf23893627092544e047825caeb4eb14a
LGarciaGonz <190077@iessanalberto.com>
2024-02-27 09:37:40 +0100

    Commit final

--- src/main/java/Code/InsertarXML.java
+++ src/main/java/Code/InsertarXML.java
@@ -11,89 +11,85 @@ public class InsertarXML {
 
     static Collection col = null;
 
-    //Metodo para insertar los datos Xml a la colecion de ExistDb
+    // Método para insertar los datos XML a la colección de ExistDb
     public static void insertXML() {
 
+        // Archivos XML que se van a insertar
         File XmlFamilia = new File("src/main/resources/familias.xml");
         File XmlProyectosFP = new File("src/main/resources/proyectosFP.xml");
         File XmlCentros = new File("src/main/resources/CentrosCFGMyS.xml");
         File XmlProyectos = new File("src/main/resources/proyectos.xml");
 
+        // Conexión a la colección ExistDb
         col = CollectionConexion.conectar();
 
         if (col != null) {
 
-            //Manera de insertar el xml
+            // Se logró la conexión
             System.out.println("\t>>> Éxito de conexión");
 
-            // Comprobar que el archivo familias.xml pueda leerse.
+            // Insertar el archivo familias.xml si es posible leerlo
             if (!XmlFamilia.canRead()) {
                 System.err.println("\n>>> Error al intentar leer el documento familias.xml");
             } else {
-
                 try {
+                    // Crear un recurso y establecer su contenido con el archivo XML
                     Resource resource = col.createResource(XmlFamilia.getName(), "XMLResource");
                     resource.setContent(XmlFamilia);
+                    // Almacenar el recurso en la colección
                     col.storeResource(resource);
                 } catch (XMLDBException e) {
-                    // Manejar el error.
+                    // Manejar el error en caso de excepción
                     System.err.println("\n>>> Error: " + e.getMessage());
                 }
-
             }
 
-            // Comprobar que el archivo proyectosFP.xml pueda leerse.
+            // Insertar el archivo proyectosFP.xml si es posible leerlo
             if (!XmlProyectosFP.canRead()) {
-
                 System.err.println("\n>>> Error al intentar leer el documento proyectosFP.xml");
-
             } else {
-
                 try {
+                    // Crear un recurso y establecer su contenido con el archivo XML
                     Resource resource = col.createResource(XmlProyectosFP.getName(), "XMLResource");
                     resource.setContent(XmlProyectosFP);
+                    // Almacenar el recurso en la colección
                     col.storeResource(resource);
                 } catch (XMLDBException e) {
-                    // Manejar el error.
+                    // Manejar el error en caso de excepción
                     System.err.println("\n>>> Error: " + e.getMessage());
                 }
-
             }
 
-            // Comprobar que el archivo CentrosCFGMyS.xml pueda leerse.
+            // Insertar el archivo CentrosCFGMyS.xml si es posible leerlo
             if (!XmlCentros.canRead()) {
-
                 System.err.println("\n>>> Error al intentar leer el documento centrosCFGMyS.csv");
-
             } else {
-
                 try {
+                    // Crear un recurso y establecer su contenido con el archivo XML
                     Resource resource = col.createResource(XmlCentros.getName(), "XMLResource");
                     resource.setContent(XmlCentros);
+                    // Almacenar el recurso en la colección
                     col.storeResource(resource);
                 } catch (XMLDBException e) {
-                    // Manejar el error.
+                    // Manejar el error en caso de excepción
                     System.err.println("\n>>> Error: " + e.getMessage());
                 }
-
             }
 
-            // Comprobar que el archivo proyectos.xml pueda leerse.
+            // Insertar el archivo proyectos.xml si es posible leerlo
             if (!XmlProyectos.canRead()) {
-
                 System.err.println("\n>>> Error al intentar leer el documento proyectos.csv");
-
             } else {
-
                 try {
+                    // Crear un recurso y establecer su contenido con el archivo XML
                     Resource resource = col.createResource(XmlProyectos.getName(), "XMLResource");
                     resource.setContent(XmlProyectos);
+                    // Almacenar el recurso en la colección
                     col.storeResource(resource);
                 } catch (XMLDBException e) {
-                    // Manejar el error.
+                    // Manejar el error en caso de excepción
                     System.err.println("\n>>> Error: " + e.getMessage());
                 }
-
             }
         }
     }
